e3021cd224fd868cf7b4eedb89a13d2294eb97e6
Dan <danpariiuni@gmail.com>
2025-11-10 09:52:27 +0100

    Llaama3.2-1B - Results

--- continual_pretrain/scripts/evaluate_model.sh
+++ continual_pretrain/scripts/evaluate_model.sh
@@ -2,14 +2,16 @@
 # This script runs tests on preliminary language understanding,
 # providing an early signal of which model understands Romanian better.
 
-export eval_batch_size=4
+export eval_batch_size=8
 export use_mps=True
-tasks_to_run=("ro_wiki")
+#tasks_to_run=("ro_wiki")
 # Convert bash array to Hydra list format: [task1,task2,task3]
 tasks_list="[$(IFS=,; echo "${tasks_to_run[*]}")]"
 
 # Define the models to test
 model_paths=("google/gemma-3-1b-it" "google/gemma-3-1b-pt")
+#model_paths=("meta-llama/Llama-3.2-1B" "meta-llama/Llama-3.2-1B-Instruct")
+
 
 for model_path in "${model_paths[@]}"; do
     echo "======================================"
@@ -17,7 +19,7 @@ for model_path in "${model_paths[@]}"; do
     echo "======================================"
     
     # Conditionally set apply_chat_template based on the model type
-    if [[ "$model_path" == *"-it" ]]; then
+    if [[ "$model_path" == *"-it" ]] || [[ "$model_path" == *"Instruct"* ]]; then
         apply_chat_template=true
     else
         apply_chat_template=false
